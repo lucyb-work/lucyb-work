4a7aba72378779e411173f239065b231fc2b31bd
Luce <76128271+lucyb-work@users.noreply.github.com>
2022-02-02 23:06:55 +0000

    Neofetch-like script
    The Bash script seen in the README.md for my lucyb-work repo

---
+++ lucefetch.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# declaring colour variables using ANSI escape codes
+DARKGREY='\033[0;37m'
+LIGHTGREY='\033[1;30m'
+WHITE='\033[1;37m'
+CYAN='\033[0;36m'
+LIGHTRED='\033[1;31m'
+
+# looks a little messy but hey-ho
+echo -e "                                             		  ${DARKGREY}OOOOOOo.	${LIGHTRED}luce@github
+                                           ${DARKGREY}..oooooooo..       OOOOO.	-----------
+                                       ${DARKGREY}.oOOOOOOOOOOOOOOOOo     OOOOO	${CYAN}Firstname: ${WHITE}Luce
+                     ${DARKGREY}..ooOOOOOOo..oooOOOOOOOOOOOOOOOOOOOOOOoooOOOOO	${CYAN}Degree: ${WHITE}Cybersecurity ${LIGHTGREY}and ${WHITE}Forensic Computing
+            ${DARKGREY}.Oo...ooOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO	${CYAN}Learning: ${WHITE}C
+        ${DARKGREY}.oooOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO~~		${CYAN}IDE: ${WHITE}VSC
+          ${DARKGREY}\oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO'		${CYAN}Languages: ${WHITE}Python, Java
+           ${DARKGREY}OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOoOOOOOOOOOOOOOOO'		${CYAN}Learning Next: ${WHITE}Bash, Powershell
+          ${DARKGREY}__\OO/     OOOOOOOOOOOOOOOOOOOO~OOOOOOOOOOOOO			${CYAN}Interests: ${WHITE}Blue-teaming ${LIGHTGREY}[Malware Analysis, Incident Response]
+            ${DARKGREY}/|\   .oOOooo- OOOOOOOO     .O^OOOOOOOOOO			${CYAN}Hobbies: ${WHITE}Lock Picking, Cooking, Reading
+  ${LIGHTGREY}oO--.        ${DARKGREY}.oOOOO~    .OOOOO       QQOO^OOOOOOOOOo
+  ${LIGHTGREY}+o--o----${DARKGREY}QQOOO~        .OOOOO                  OOOOO
+                       ${DARKGREY}.OOOO                   QQQQO^
+                     ${DARKGREY}QQQQO"
+                     
+             
