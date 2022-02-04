464d8e9ebea2992fd8f64c0cf0eea433c47d5b2a
Luce <76128271+lucyb-work@users.noreply.github.com>
2022-02-04 14:17:34 +0000

    complete overhaul

--- lucefetch.sh
+++ lucefetch.sh
@@ -5,22 +5,18 @@ DARKGREY='\033[0;37m'
 LIGHTGREY='\033[1;30m'
 WHITE='\033[1;37m'
 CYAN='\033[0;36m'
-LIGHTRED='\033[1;31m'
+#LIGHTRED='\033[1;31m'
+PURPLE='\033[1;35m'
 
 # looks a little messy but hey-ho
-echo -e "                                             		  ${DARKGREY}OOOOOOo.	${LIGHTRED}luce@github
-                                           ${DARKGREY}..oooooooo..       OOOOO.	-----------
-                                       ${DARKGREY}.oOOOOOOOOOOOOOOOOo     OOOOO	${CYAN}Firstname: ${WHITE}Luce
-                     ${DARKGREY}..ooOOOOOOo..oooOOOOOOOOOOOOOOOOOOOOOOoooOOOOO	${CYAN}Degree: ${WHITE}Cybersecurity ${LIGHTGREY}and ${WHITE}Forensic Computing
-            ${DARKGREY}.Oo...ooOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO	${CYAN}Learning: ${WHITE}C
-        ${DARKGREY}.oooOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO~~		${CYAN}IDE: ${WHITE}VSC
-          ${DARKGREY}\oOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO'		${CYAN}Languages: ${WHITE}Python, Java
-           ${DARKGREY}OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOoOOOOOOOOOOOOOOO'		${CYAN}Learning Next: ${WHITE}Bash, Powershell
-          ${DARKGREY}__\OO/     OOOOOOOOOOOOOOOOOOOO~OOOOOOOOOOOOO			${CYAN}Interests: ${WHITE}Blue-teaming ${LIGHTGREY}[Malware Analysis, Incident Response]
-            ${DARKGREY}/|\   .oOOooo- OOOOOOOO     .O^OOOOOOOOOO			${CYAN}Hobbies: ${WHITE}Lock Picking, Cooking, Reading
-  ${LIGHTGREY}oO--.        ${DARKGREY}.oOOOO~    .OOOOO       QQOO^OOOOOOOOOo
-  ${LIGHTGREY}+o--o----${DARKGREY}QQOOO~        .OOOOO                  OOOOO
-                       ${DARKGREY}.OOOO                   QQQQO^
-                     ${DARKGREY}QQQQO"
-                     
-             
+          
+echo -e "
+     					${PURPLE}luce@github
+                            ${DARKGREY}__		-----------
+     ${DARKGREY},                    ,\" e\`--o	${CYAN}Firstname: ${WHITE}Luce
+    ${DARKGREY}((                   (  | __,'	${CYAN}Degree: ${WHITE}BSc Cybersecurity ${DARKGREY}and ${WHITE}Forensic Computing
+     ${DARKGREY}\~~----------------' \_;/		${CYAN}Learning: ${WHITE}C
+     ${DARKGREY}(                      /		${CYAN}Languages: ${WHITE}Python, Java
+     ${DARKGREY}/) ._______________.  )		${CYAN}Next: ${WHITE}Bash, Powershell
+    ${DARKGREY}(( (               (( (		${CYAN}Interests: ${WHITE}Blue-teaming ${DARKGREY}[Malware Analysis, Incident Response]
+     ${DARKGREY}\`\`-'               \`\`-'		${CYAN}Hobbies: ${WHITE}Lock Picking, Cooking, Reading\n"
